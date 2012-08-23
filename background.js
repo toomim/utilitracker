98c20d872d73ebf9b046b03788ff38e1a42f6c42
Eric Zeng <ericzeng2@gmail.com>
2012-08-23 12:45:59 -0700

    Adding my local files

---
+++ background.js
@@ -0,0 +1,101 @@
+// "Main" function - checks for blocked sites whenever a tab is updated.
+// Redirects to our block page. 
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+	if(isBlocked(getHostname(tab.url))){
+		chrome.tabs.update(tab.id, 
+		{ "url" : chrome.extension.getURL("blocked.html") + "?url=" + escape(tab.url) });
+	}
+});
+
+
+// Extracts hostname from the URL
+// eg: https://www.google.com/webhp?hl=en&tab=nw&authuser=0 -> www.google.com
+function getHostname(str) {
+	var re = new RegExp('^(?:f|ht)tp(?:s)?\://([^/]+)', 'im');
+	return str.match(re)[1].toString();
+}
+
+function getBlocked(){
+	// Replace with code to get sites from our server
+	var urls = ["bing.com", "bbc.co.uk"];
+	return urls;
+}
+
+// Finds blocked urls from a given url.
+// Returns true if given url is blocked, false if otherwise. 
+function isBlocked(url){
+	var blocked = getBlocked();
+	for(var i = 0; i < blocked.length; i++){
+		var blockUrl = blocked[i];
+		if (url.search(blockUrl) != -1){
+			return true;
+		}
+	}
+	return false;
+}
+
+// Stores url, time/date of block in localStorage
+function storeBlockData(event, user, tab_url, value) {
+	// Get the time of block
+	var time = new Date();
+	var month = time.getMonth() + 1;
+	var day = time.getDate();
+	var year = time.getFullYear();
+	var hour = time.getHours();
+	var minute = time.getMinutes();
+	var second = time.getSeconds();
+	
+	// Formatting time and date
+	var timeDate = month + "/" + day + "/" + year + " ";
+	
+	if(minute < 10){
+		minute = "0" + minute;
+	}
+	if(second < 10){
+		second = "0" + second;
+	}
+	
+	timeDate = timeDate + hour + ":" + minute + ":" + second;
+	if(localStorage['log'] == undefined){
+		localStorage['log'] = JSON.stringify([]);
+	}
+	
+	// Stores data online
+	postToServer(event, user, timeDate, tab_url, value);
+	
+	// Stores second copy in localStorage
+	var array = JSON.parse(localStorage['log']);
+	array.push(event, user, timeDate, tab_url, value);
+	localStorage['log'] = JSON.stringify(array);
+}
+
+// Pushes data about site blocks to the server:
+// Type of block, user info, time, url, and surveyed value
+function postToServer(event, user, timeDate, url, value) {
+	
+	// another solution:
+	var xmlHttp = new XMLHttpRequest();
+	var tourl = "http://yuno.us:8989/save_event";
+	var params = 
+		"paid=" + value +
+		"&what=" + event +  
+		"&who=" + user + 
+		"&when=" + timeDate +
+		"&url=" + url;
+	xmlHttp.open("POST", tourl, false);
+	
+	//Send the proper header information along with the request  //x-www-form-urlencoded
+	xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xmlHttp.setRequestHeader("Content-length", params.length);
+	xmlHttp.setRequestHeader("Connection", "close");
+
+	xmlHttp.onreadystatechange = function() {//Call a function when the state changes.
+	    if(xmlHttp.readyState == 4) {
+	        // alert(xmlHttp.statusText);
+			if(xmlHttp.status == 200) {
+				console.log(xmlHttp.responseText);
+			}
+	    }
+	}
+	xmlHttp.send(params);
+}
