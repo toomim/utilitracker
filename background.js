b31522c34c53807270cdc7d46f8d1fbc8545764f
Siye Liu <lius4@uw.edu>
2012-10-04 00:11:18 -0700

    fix bugs and printing information to console when blocking page.
    the extension can actually block the websites in the urls array, but it
    will always blocking.

--- background.js
+++ background.js
@@ -1,58 +1,78 @@
 // Options
-var urls = ['bing.com', 'facebook.com', 'reddit.com', 'renren.com'];
+var urls = ['www.bing.com', 'facebook.com', 'reddit.com', 'renren.com'];
 var user = "Debug";
-
 
 // State variables
 var sites = urls.map(function (url) {
     return {url_pattern: url, our_offer: null, user_offer: null}; });
 
-// New day check
+// New day check /24 hrs later
 var last_day_check = new Date();
 function check_for_new_day () {
-    // Check to see if it's a new day
+    // Check to see if it's a new day/ over 24 hours
 	var today_time = new Date();
-	if(today_time.toDateString() != last_day_check.toDateString()) {
+	
+	if(today_time.getTime() - last_day_check.getTime() >= (1000 * 60 * 60 * 24)) {
         // If so, reset offers
         sites.each(function (site) {
             site.our_offer = null;
             site.user_offer = null; });
-	}
-    last_day_check = new Date();
+	
+        }
+    last_day_check = today_time;
     // and go through all tabs and re-block what's needed
+    
+    console.log('should check tabs and reblock');
 }
-
 
 // "Main" function - checks for blocked sites whenever a tab is updated.
 // Redirects to our block page. 
-function tab_event_listener (tab_id, change_info, tab) {
-    console.log('tab_event_listener of', tab);
+function tabs_update_listener(tab_id, change_info, tab) {
+    console.log('tab_updated ', tab.id);    
 	if (is_blocked(tab.url)) {
 		// Redirect tab to blocked.html
 		chrome.tabs.update(tab.id, 
-			{ "url" : chrome.extension.getURL("blocked.html")
+			{ 'url' : chrome.extension.getURL("blocked.html")
               + "?url=" + escape(tab.url) });
 
         // Record the block event
 		store_block_data("block", get_username(), get_url(), null);
-	}
+	}	
 }
-chrome.tabs.onUpdated.addListener(tab_event_listener);
+function tabs_created_listener(tab) {
+	console.log('tab_created', tab.id);
+	if (is_blocked(tab.url)) {
+		// Redirect tab to blocked.html
+		chrome.tabs.update(tab.id, 
+			{ 'url' : chrome.extension.getURL("blocked.html")
+              + "?url=" + escape(tab.url) });
+
+        // Record the block event
+		store_block_data("block", get_username(), get_url(), null);
+	}		
+}
+
+chrome.tabs.onUpdated.addListener(tabs_update_listener);
+
+chrome.tabs.onCreated.addListener(tabs_created_listener);
+
+chrome.tabs.onRemoved.addListener(function(tab) {
+	console.log('tab_removed ', tab.id);
+});
+
 
 // Extracts hostname from the URL
 // eg: https://www.google.com/webhp?hl=en&tab=nw&authuser=0 -> www.google.com
 function get_hostname(str) {
-	var re = new RegExp('^(?:f|ht)tp(?:s)?\://([^/]+)', 'im');
-	return str.match(re)[1].toString();
+	var exp = str.split('//')[1].split('/')[0].substr(4);
+	return exp;
 }
 
 function site_for(url) {
     // We just care about the hostname
     url = get_hostname(url);
 
-    return sites.find(function (site) {
-		return url.search(site.url_pattern) != -1;
-    });
+	return urls.indexOf(url);
 }
 
 function is_blocked(url) {
@@ -60,9 +80,24 @@ function is_blocked(url) {
     //    • The user has not given an offer yet
     //    • Or they did, and it was less than our offer (so we gave them
     //      our offer instead)
-    var site = site_for(url);
-    return site && !site.user_offer || site.user_offer < site_our_offer;
+    
+
+    var site = get_hostname(url);
+	console.log('is_blocked', site);
+	
+	// check whether this url is blocked right now
+	var boo = !!sites.find(function(this_site) {
+		return this_site.url_pattern == site && !this_site.user_offer;	
+	});
+	console.log('is_blocked', boo);
+
+	return boo;
 }
+
+function get_username() {
+	return user;
+}
+
 
 // Stores url, time/date of block in localStorage
 function store_block_data(event, user, tab_url, value) {
