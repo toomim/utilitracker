0f53e4bffc2f1c358773495300358cd4a717feb0
Siye Liu <lius4@uw.edu>
2012-10-06 15:38:00 -0700

    add icon on browser for extension

---
+++ popup.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", onload);
+var background = chrome.extension.getBackgroundPage();
+function onload() {
+	var status = background.get_data("urls_status");
+	var ul = document.createElement('ul');
+	for(var i = 0; i < status.length; i++) {
+	    var tmp = document.createElement('li'); 
+	    tmp.innerHTML = status[i].url_pattern;
+		ul.appendChild(tmp);		
+	}
+	document.getElementById('data_part').appendChild(ul);
+}
