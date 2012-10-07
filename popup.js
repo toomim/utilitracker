382935532edd358a24d38d3e1b724016856b8d12
Siye Liu <lius4@uw.edu>
2012-10-07 13:09:51 -0700

    let the balloon show the current user_offer for each website

--- popup.js
+++ popup.js
@@ -5,7 +5,7 @@ function onload() {
 	var ul = document.createElement('ul');
 	for(var i = 0; i < status.length; i++) {
 	    var tmp = document.createElement('li'); 
-	    tmp.innerHTML = status[i].url_pattern;
+	    tmp.innerHTML = status[i].url_pattern + ' user_offer: ' + status[i].user_offer;
 		ul.appendChild(tmp);		
 	}
 	document.getElementById('data_part').appendChild(ul);
