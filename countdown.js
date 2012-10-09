61d669adfcb409343e784ef41a5229041d2c2c33
Siye Liu <lius4@uw.edu>
2012-10-08 23:56:47 -0700

    implement the countdown timer

--- countdown.js
+++ countdown.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", onload, false);
 var background = chrome.extension.getBackgroundPage();
+var timer = null;
 function onload() {
 	// Parses the extension url to get the incoming url
 	var ws = window.location.search;
@@ -18,10 +19,9 @@ function onload() {
 		}
 		
 		// set the remaining time div
-		var click_time = get_remaining_time(url);
-		if(click_time != 'not clicked') {
-		    var click_date = new Date(click_time);
-		    document.getElementById('remaining_time').innerHTML = click_date;
+		var remain_time = get_remaining_time(url);
+		if(remain_time != 'not clicked') {
+            countdown(remain_time);
 		}
 	}	
 }
@@ -32,34 +32,43 @@ function get_remaining_time(url) {
 		for(var i = 0; i < temp_data.length; i++) {
             		if(url.indexOf(temp_data[i].url_pattern) != -1) {
             		    // found the data
-            		    return temp_data[i].last_day_check;
+            		    var now = new Date();
+            		    var passed = now.getTime() - temp_data[i].last_day_check;
+            		    console.log('passed: ' + passed);
+            		    console.log('remaining' + (60*60*24*1000 - passed) / 1000);
+            		    return ((60*60*24*1000 - passed) / 1000);
             		}
 		}
 		// cannot find it;
 		return ('not clicked');
 }
 
-function set_timer() {
-    var now = new Date();
-    var theevent = new Date("Sep 29 2007 00:00:01");
-    var seconds = (theevent - now) / 1000;
-    var minutes = seconds / 60;
-    var hours = minutes / 60;
-    var days = hours / 24;
-    ID=window.setTimeout("update();", 1000);
-    function update() {
-    now = new Date();
-    seconds = (theevent - now) / 1000;
-    seconds = Math.round(seconds);
-    minutes = seconds / 60;
-    minutes = Math.round(minutes);
-    hours = minutes / 60;
-    hours = Math.round(hours);
-    days = hours / 24;
-    days = Math.round(days);
-    document.form1.days.value = days;
-    document.form1.hours.value = hours;
-    document.form1.minutes.value = minutes;
-    document.form1.seconds.value = seconds;
-    ID=window.setTimeout("update();",1000);
+function countdown(total_seconds) {
+	if (!timer) {
+    	document.getElementById('remaining_hours').innerHTML = parseInt(((total_seconds / 60) / 60) % 24);
+    	document.getElementById('remaining_minutes').innerHTML = parseInt((total_seconds / 60) % 60);
+    	document.getElementById('remaining_seconds').innerHTML = total_seconds % 60;
+	    
+		timer = setInterval(oneSecond, 1000);
+	}
+}
+
+function oneSecond() {
+    var hrs = parseInt(document.getElementById('remaining_hours').innerHTML);
+	var min = parseInt(document.getElementById('remaining_minutes').innerHTML);
+	var sec = parseInt(document.getElementById('remaining_seconds').innerHTML);
+	var total_seconds = (hrs * 60 + min) * 60 + sec;
+	console.log(total_seconds);
+	if (total_seconds <= 1) {
+		// time's up!
+		clearInterval(timer);
+		timer = null;
+        total_seconds--;
+        // trying to go through
+	} else {
+        total_seconds--;
+	}
+	document.getElementById('remaining_hours').innerHTML = parseInt(((total_seconds / 60) / 60) % 24);
+	document.getElementById('remaining_minutes').innerHTML = parseInt((total_seconds / 60) % 60);
+	document.getElementById('remaining_seconds').innerHTML = total_seconds % 60;
 }
