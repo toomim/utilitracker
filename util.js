777bd443cc489f821d7bf1251a9239c1e6db057b
Michael Toomim <toomim@gmail.com>
2012-10-11 18:18:48 -0700

    renamed files

--- util.js
+++ util.js
@@ -61,3 +61,51 @@ function hash_each (h,f) {
         if (h.hasOwnProperty(key))
             f(key,h[key]);
 };
+
+
+
+// Storage Read and Write
+
+var store = {
+    read : function() {
+        var stg = localStorage;
+
+        // Load options from localStorage
+        var opts = JSON.parse(stg['urg_data'] || '{}')
+
+        // Now replace defaults that haven't been set yet
+	    if (! ("user" in opts))
+		    opts.username = "default_user";
+		  
+	    if (! ("urls" in opts))
+		    opts.urls = [];
+				  
+	    if (! ("urls_status" in opts))
+		    opts.urls_status = {};
+		  
+	    if (! ("real_money" in opts))
+	        opts.real_money = 'false';
+		  
+        return opts;
+    },
+
+    write : function(opts, stg) {
+        var stg = localStorage;
+
+        stg['urg_data'] = JSON.stringify(opts);
+    }
+};
+
+// retrieve data from localStorage
+function get_data(key) {
+	// return JSON.parse(localStorage.getItem(key));
+	return store.read()[key];
+}
+
+// store data to localStorage
+function set_data(key, value) {
+	// localStorage.setItem(key, JSON.stringify(value));	
+	var temp_data = store.read();
+    temp_data[key] = value;
+	store.write(temp_data);	
+}
