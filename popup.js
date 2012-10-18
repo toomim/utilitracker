cf3a8c9239332d2df8762b09c13ad2645b12bce0
Siye Liu <lius4@uw.edu>
2012-10-17 21:01:08 -0700

    change the popup balloon to show only visited sites

--- popup.js
+++ popup.js
@@ -2,16 +2,23 @@ document.addEventListener("DOMContentLoaded", onload);
 var background = chrome.extension.getBackgroundPage();
 
 function onload() {
+    load_visited_sites();
+    document.getElementById('reset_data').onclick = clear_data;
+}
+
+function load_visited_sites() {
 	var status = background.get_data("website_state");
 	var ul = document.createElement('ul');
-	for(var i = 0; i < status.length; i++) {
-	    var tmp = document.createElement('li'); 
-	    tmp.innerHTML = status[i].url_pattern + ' user_offer: ' + status[i].user_offer + ' last_day_check: ' + status[i].last_day_check;
-		ul.appendChild(tmp);		
-	}
-	document.getElementById('data_part').appendChild(ul);
-
-    document.getElementById('reset_data').onclick = clear_data;
+	
+	var states = get_data('website_state');
+    states.each(function (state) {
+		if(state.user_offer != null) {
+        	var tmp = document.createElement('li'); 
+        	tmp.innerHTML = state.url_pattern + ' user_offer: ' + state.user_offer + ' last_day_check: ' + state.last_day_check;
+        	ul.appendChild(tmp);		
+		}
+    });
+	document.getElementById('data_part').appendChild(ul);    
 }
 
 
