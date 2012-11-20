2d9bb4f3f8a54e87cbab7425da91e19f651c4acb
Siye Liu <lius4@uw.edu>
2012-11-19 23:24:04 -0800

    merge two pages: ask_offer and countdown into one page. almost done

--- ask_offer.js
+++ ask_offer.js
@@ -7,6 +7,7 @@ var variants = [['<div class="subtitle">Sell your <a class="url"></a> access for
                 ['<div class="subtitle">Sell your <a class="url"></a> access for 24 hours.<br>Name your price.</div><br><br>',
                  '<img src="lock_dim.png" class="lock">'],
 ]
+var timer = null;
 
 /*,
                 ['How much money would we have to give you for you to not have access to <a class="url"></a> for 24 hours?', '']],
@@ -20,11 +21,6 @@ var variants = [['<div class="subtitle">Sell your <a class="url"></a> access for
 // being blocked by Chrome for security reasons
 document.addEventListener("DOMContentLoaded", onload, false);
 
-document.addEventListener('DOMContentLoaded', function () {
-	document.querySelector('body').addEventListener('keypress', keyboard_submit);
-	document.querySelector('#continueButton').addEventListener('click', clickHandler);
-});
-
 // Called by the event listener when the page loads
 function onload() {
 	// Parses the extension url to get the incoming url
@@ -34,19 +30,19 @@ function onload() {
 		for (var i = 0; i < kvs.length; i++) {
 			var kv = kvs[i].split('=');
 			if (kv[0] == 'url') {
-				var url_objs = document.getElementsByClassName("url");
-				for (var j=0; j<url_objs.length; j++) {
-					var url = decodeURIComponent(kv[1]);
-					var site_name = get_hostname(url);
-					url_objs[j].href = url;
-					url_objs[j].appendChild(document.createTextNode(site_name));
-				}
+				var url = decodeURIComponent(kv[1]);
 			}
 		}
 	}	
-
+	
 	site = find_website_state(url);
 	if(site.user_offer == null) {
+		// add event listener
+		document.addEventListener('DOMContentLoaded', function () {
+			document.querySelector('body').addEventListener('keypress', keyboard_submit);
+			document.querySelector('#continueButton').addEventListener('click', clickHandler);
+		});
+		
 		// this is the ask_offer step
 		console.log('this is a ask offer');
 		// Set up the a/b test
@@ -57,7 +53,7 @@ function onload() {
     	set_data('variant', (v+1) % variants.length);
     	document.getElementById('prompt1').innerHTML = "<img src='ask_offer_title.png' />";
     	document.getElementById('prompt2').innerHTML = variants[v][0];
-    
+    	set_url();
     	/*var tmp = 'variations: '
     	for (var i=0; i<variants.length; i++)
     		tmp += '<a href="#">' + i + '</a> '
@@ -77,14 +73,44 @@ function onload() {
     
     	console.log('ask_offer.js loaded');
 	} else {
+		set_url();
 		// this is the countdown step
 		console.log('this is a countdown');	
-		var tit = document.getElementById('prompt').innerHTML = "CONGRAT! TODAY'S AWARD IS $<span id='our_offer'>00.00</span> !";
+		var tit = document.getElementById('prompt1');
+		tit.innerHTML = "CONGRAT! TODAY'S AWARD IS $<span id='our_offer'>00.00</span> !";
+		tit.style.color = "black";
 		tit.style.opacity = 1;
-		var cdt = document.getElementById('ask').innerHTML = "<div id='remaining_time'><span id='remaining_hours'></span>:<span id='remaining_minutes'></span>:<span id='remaining_seconds'></span></div>";
+		var cdt = document.getElementById('ask');
+		cdt.innerHTML = "<div id='remaining_time'><span id='remaining_hours'></span>:<span id='remaining_minutes'></span>:<span id='remaining_seconds'></span></div>";
 		cdt.style.marginTop = '0px';
+		// set the remaining time div
+		var remain_time = get_remaining_time(url);
+		if(remain_time != 'not clicked') {
+			countdown(remain_time);
+		}
+
 	}
 
+}
+
+function set_url () {
+	// Parses the extension url to get the incoming url
+	var ws = window.location.search;
+	if (ws !== undefined) {
+		var kvs = ws.substring(1).split('&');
+		for (var i = 0; i < kvs.length; i++) {
+			var kv = kvs[i].split('=');
+			if (kv[0] == 'url') {
+				var url_objs = document.getElementsByClassName('url');
+				for (var j=0; j<url_objs.length; j++) {
+					var url = decodeURIComponent(kv[1]);
+					var site_name = get_hostname(url);
+					url_objs[j].href = url;
+					url_objs[j].appendChild(document.createTextNode(site_name));
+				}
+			}
+		}
+	}	
 }
 
 // Submits the value inputted by the user to the server. 
@@ -214,7 +240,7 @@ function sliding_down() {
 }
 
 function get_remaining_time(url) {
-		var temp_data = background.get_data('website_state');
+		var temp_data = get_data('website_state');
 		for(var i = 0; i < temp_data.length; i++) {
 					if(url.indexOf(temp_data[i].url_pattern) != -1) {
 						// found the data
